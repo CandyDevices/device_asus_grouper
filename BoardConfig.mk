e91d544f28924b8788ac3b9cb3d5789babe074fa
Dennis S Lebhardt jr. <dennislebhardt@yahoo.com>
2015-02-14 15:36:58 -0600

    s

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,6 +38,10 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/asus/grouper
 -include vendor/asus/grouper/BoardConfigVendor.mk
 include device/asus/grouper/BoardConfigCommon.mk
 
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8-sm/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+SUGARRUSH=true
+
 TARGET_RECOVERY_FSTAB = device/asus/grouper/fstab.grouper
 
 MALLOC_IMPL := dlmalloc
